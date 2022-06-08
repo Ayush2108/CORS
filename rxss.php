8e669e2ab329c8d0340291f88813c6d06f272105
ayush1098 <sourabhsingh246891@gmail.com>
2022-06-08 20:30:21 +0530

    Ayush

--- rxss.php
+++ rxss.php
@@ -2,10 +2,11 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="UTF-8">
+	<meta name="referrer" content="unsafe-url"> 
 <title>Referer based XSS testing</title>
 </head>
 <body>
-	 <meta name="referrer" content="unsafe-url"> 
 <script>window.location.replace('<?php echo $_GET['target']; ?>');</script>
 </body>
 </html>
